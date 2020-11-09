4d9637ed2e196951d1cb918dbfc6e57fe1d4f62c
zhangdh <zhangdonghui@cmict.chinamobile.com>
2020-11-09 11:01:46 +0800

    git commit

---
+++ src/main/java/com/example/myConntroller.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ *
+ * @author zhangdh
+ * @version 1.0
+ * @date 2020/11/9 10:56
+ */
+
+@RestController
+public class myConntroller {
+
+    @GetMapping("/get")
+    public String get(){
+        return "test1";
+    }
+}
